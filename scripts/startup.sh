4e76680f7ceabd050271a6b4d3834b73f9787772
Ian Rose <ianrose14@gmail.com>
2023-11-10 13:35:50 -0500

    use GCR in startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,7 +6,7 @@ set -e
 (podman rm webapp) || true
 
 gcloud auth --quiet print-access-token | podman login -u oauth2accesstoken --password-stdin https://us-central1-docker.pkg.dev
-podman pull us-central1-docker.pkg.dev/ian-rose/docker-1/webapp:latest
+docker pull gcr.io/ian-rose/us-central1-docker.pkg.dev/ian-rose/docker-1/webapp:latest
 podman run -d -p 8080:80 -p 8443:443 --restart=no \
     -v "$(pwd)/config":/root/config/ -v "$(pwd)/data":/root/data/ --name webapp \
-    us-central1-docker.pkg.dev/ian-rose/docker-1/webapp:latest /root/webapp -host ianthomasrose.com
+    gcr.io/ian-rose/us-central1-docker.pkg.dev/ian-rose/docker-1/webapp:latest /root/webapp -host ianthomasrose.com
